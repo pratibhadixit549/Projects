41a39132d6e04ea043d0b1f282b3f157bf7f7ef4
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 03:10:51 +0530

    print statement declaring the that always player "X" will get the first chance declared

--- src/TicTacToe/TicTacToeGame.java
+++ src/TicTacToe/TicTacToeGame.java
@@ -21,8 +21,8 @@ public class TicTacToeGame {
 
         System.out.println("Welcome to 2 Player Tic Tac Toe.");
         System.out.println("--------------------------------");
-
         printBoard();
+        System.out.println("X's will play first. Enter a slot number to place X in:");
 
 
     }

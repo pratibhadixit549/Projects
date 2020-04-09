07ca5245d816f685b6568fd6cd8c4220e8c83c94
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 02:37:06 +0530

    by the object of CoffeeMachineProcedure class we called the ready method

--- src/CoffeeMachine/main/CoffeeMachineMain.java
+++ src/CoffeeMachine/main/CoffeeMachineMain.java
@@ -5,7 +5,7 @@ import CoffeeMachine.coffeeMachineProcedure.CoffeeMachineProcedure;
 public class CoffeeMachineMain {
     public static void main(String[] args) {
         CoffeeMachineProcedure objectOfCoffeeMachineProcedure = new CoffeeMachineProcedure(400, 540, 120, 9, 550);
-
-
+        objectOfCoffeeMachineProcedure.ready();
+        // ...
     }
 }

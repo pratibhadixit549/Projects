cfdab8ff5e71dd6fbf4c3f4a1cd6cdd1a0f76a75
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 03:08:54 +0530

    print statements declared in the printBoard() method of the TicTacToeGame class

--- src/TicTacToe/TicTacToeGame.java
+++ src/TicTacToe/TicTacToeGame.java
@@ -28,6 +28,14 @@ public class TicTacToeGame {
     }
 
     private static void printBoard() {
+        System.out.println("/---|---|---\\");
+        System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+        System.out.println("/---|---|---\\");
+        // ...
     }
 
     private static void populateEmptyBoard() {

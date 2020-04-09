c866e7f7de26c55c027001330345fab033858862
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 02:51:04 +0530

    first we will always consider the turn of player "X" hence a variable of type String declared named turn

--- src/TicTacToe/TicTacToeGame.java
+++ src/TicTacToe/TicTacToeGame.java
@@ -5,4 +5,5 @@ import java.util.Scanner;
 public class TicTacToeGame {
     static Scanner scanner = new Scanner(System.in);
     static String[] board = new String[9];
+    static String turn = "X";
 }

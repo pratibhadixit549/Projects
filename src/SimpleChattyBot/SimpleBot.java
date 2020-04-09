6a63f8028d8af36dfe7cfb083e731e0fd253f379
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 01:08:52 +0530

    method count() of void return type completed

--- src/SimpleChattyBot/SimpleBot.java
+++ src/SimpleChattyBot/SimpleBot.java
@@ -13,6 +13,11 @@ public class SimpleBot {
     }
 
     private static void count() {
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.printf("%d!\n", i);
+        }
     }
 
     private static void guessAge() {

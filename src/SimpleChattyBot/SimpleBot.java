d797e38efa0a0f71c38bf816621677451838324d
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 01:00:38 +0530

    a new method greet() declared consisting of assistantName and birthYear and of void return type

--- src/SimpleChattyBot/SimpleBot.java
+++ src/SimpleChattyBot/SimpleBot.java
@@ -4,4 +4,11 @@ import java.util.Scanner;
 
 public class SimpleBot {
     final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        greet("Aid", "2018");
+    }
+
+    private static void greet(String assintantName, String birthYear) {
+    }
 }

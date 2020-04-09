af29ebe0b68d25606933fdc7098a3b0d31c6238b
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 03:30:02 +0530

    switch-case in the checkwinner() method of TicTacToeGame class completed

--- src/TicTacToe/TicTacToeGame.java
+++ src/TicTacToe/TicTacToeGame.java
@@ -57,7 +57,30 @@ public class TicTacToeGame {
         for (int i = 0; i < 8; i++) {
             String line = null;
             switch (i) {
-
+                case 0:
+                    line = board[0] + board[1] + board[2];
+                    break;
+                case 1:
+                    line = board[3] + board[4] + board[5];
+                    break;
+                case 2:
+                    line = board[6] + board[7] + board[8];
+                    break;
+                case 3:
+                    line = board[0] + board[3] + board[6];
+                    break;
+                case 4:
+                    line = board[1] + board[4] + board[7];
+                    break;
+                case 5:
+                    line = board[2] + board[5] + board[8];
+                    break;
+                case 6:
+                    line = board[0] + board[4] + board[8];
+                    break;
+                case 7:
+                    line = board[2] + board[4] + board[6];
+                    break;
             }
         }
         return "";

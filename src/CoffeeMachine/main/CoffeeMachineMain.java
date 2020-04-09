f99f6f650d727f4ca3349909c043f2663f283a0a
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 02:37:34 +0530

    CoffeeMachineMain class completed

--- src/CoffeeMachine/main/CoffeeMachineMain.java
+++ src/CoffeeMachine/main/CoffeeMachineMain.java
@@ -6,6 +6,5 @@ public class CoffeeMachineMain {
     public static void main(String[] args) {
         CoffeeMachineProcedure objectOfCoffeeMachineProcedure = new CoffeeMachineProcedure(400, 540, 120, 9, 550);
         objectOfCoffeeMachineProcedure.ready();
-        // ...
     }
 }

4e9e26432752cf3cca1b4853367845cab54e61fa
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 03:40:41 +0530

    if statement in the main() method of TicTacToeGame class completed

--- src/TicTacToe/TicTacToeGame.java
+++ src/TicTacToe/TicTacToeGame.java
@@ -47,7 +47,6 @@ public class TicTacToeGame {
                 }
                 printBoard();
                 winner = checkWinner();
-                // ...
             } else {
 
             }

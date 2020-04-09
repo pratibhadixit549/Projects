74ef0c4b7b1ea5a3f74dfddc2231e6257ef276af
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 01:07:12 +0530

    method guessAge() of void return type completed

--- src/SimpleChattyBot/SimpleBot.java
+++ src/SimpleChattyBot/SimpleBot.java
@@ -12,7 +12,13 @@ public class SimpleBot {
     }
 
     private static void guessAge() {
-
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
     }
 
     private static void remindName() {

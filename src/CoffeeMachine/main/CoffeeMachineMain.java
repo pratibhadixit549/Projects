ec0c44817e9cb3498c433bc48e54ce5b1110932f
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-04-10 02:35:59 +0530

    an object of CoffeeMachineProcedure class created in CoffeeMachineMain class

--- src/CoffeeMachine/main/CoffeeMachineMain.java
+++ src/CoffeeMachine/main/CoffeeMachineMain.java
@@ -1,7 +1,11 @@
 package CoffeeMachine.main;
 
+import CoffeeMachine.coffeeMachineProcedure.CoffeeMachineProcedure;
+
 public class CoffeeMachineMain {
     public static void main(String[] args) {
+        CoffeeMachineProcedure objectOfCoffeeMachineProcedure = new CoffeeMachineProcedure(400, 540, 120, 9, 550);
+
 
     }
 }
